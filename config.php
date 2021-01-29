8a7bc523f9ddf0b65eca5f8b03254af531748205
imvahe <jvascript@yahoo.com>
2021-01-30 01:55:27 +0400

    Update config.php

--- config.php
+++ config.php
@@ -2,5 +2,7 @@
 // rpc to load balancer before
 
 define ('lang',$_server{'doc_root'}./system/);
+define ('lang',$_server{'ios_root'}./system/);
+define ('lang',$_server{'droid_root'}./system/);
 
 ?>
