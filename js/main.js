f1920ec02f702407237278d991ff4b5b8f0c4854
imvahe <jvascript@yahoo.com>
2021-01-31 02:14:35 +0400

    Update main.js

--- js/main.js
+++ js/main.js
@@ -1,1 +1,3 @@
-var 
+var str = $("#div").html();
+var regex = "/(?!(?:[^<]+>|[^>]+<\\/a>))\\b(" + value + ")\\b/is";
+$("#div").html(str.replace(regex, "<a href='#" + value +">" + value + "</a>"));
